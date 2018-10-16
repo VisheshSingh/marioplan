53caae114da82c320185f9ed6ff23df3a8add733
Vishesh <visheshsingh16@gmail.com>
2018-10-16 19:46:22 -0400

    add dummy data to reducer, binding the component and passsing data through props

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
-  return (
-    <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-    </div>
+const ProjectList = props => {
+  const { projects } = props;
+  const projectList = projects.length ? (
+    projects.map(project => {
+      return <ProjectSummary project={project} key={project.id} />;
+    })
+  ) : (
+    <p>No project yet...</p>
   );
+
+  return <div className="project-list section">{projectList}</div>;
 };
 
 export default ProjectList;
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,22 @@
-var initState = {};
+var initState = {
+  projects: [
+    {
+      id: 1,
+      title: "help me find peach",
+      content: "lorem ipsum is a random greek characters"
+    },
+    {
+      id: 2,
+      title: "collect all stars",
+      content: "i don't know what i am typing"
+    },
+    {
+      id: 3,
+      title: "egg hunt with yoshi",
+      content: "this is a peace of dummy data"
+    }
+  ]
+};
 const projectReducer = (state = initState, action) => {
   return state;
 };
